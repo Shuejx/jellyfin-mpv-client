00a183cc132316f735929979e87ccb15f3b596d0
Shuejx <87698086+Shuejx@users.noreply.github.com>
2026-04-23 13:18:16 +0530

    Fix import statement formatting in MainActivity.kt

--- app/src/main/java/com/custom/jellyfinmpv/MainActivity.kt
+++ app/src/main/java/com/custom/jellyfinmpv/MainActivity.kt
@@ -1,16 +1,11 @@
 package com.custom.jellyfinmpv
+
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import is.xyz.mpv.MPVLib
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        MPVLib.create(this)
-        MPVLib.init()
-    }
-    override fun onDestroy() {
-        super.onDestroy()
-        MPVLib.destroy()
     }
 }
